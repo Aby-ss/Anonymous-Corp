40e56717ffcf8d001c0a8ce21de5df418ddc0cea
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-05-25 16:30:36 +0400

    Added Layout to Application ✅

--- Application/app.py
+++ Application/app.py
@@ -19,3 +19,56 @@ from rich.progress import Progress
 
 from rich.traceback import install
 install(show_locals=True)
+
+layout = Layout()
+
+layout.split_column(
+    Layout(name = "Header"),
+    Layout(name = "Body"),
+    Layout(name = "Footer")
+)
+
+layout["Body"].split_column(
+    Layout(name = "Upper_Body"),
+    Layout(name = "Lower_Body")
+)
+
+layout["Upper_Body"].split_row(
+    Layout(name = "UB_1"),
+    Layout(name = "UB_2")
+)
+
+layout["Lower_Body"].split_row(
+    Layout(name = "LB_1"),
+    Layout(name = "LB_2")
+)
+
+class Header:
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="left")
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "📰", "[b]Anonymous-Corp[/]", datetime.now().ctime().replace(":", "[blink]:[/]"),
+        )
+        return Panel(grid, style="bold white")
+    
+class Footer:
+
+    def __rich__(self) -> Panel:
+        grid = Table.grid(expand=True)
+        grid.add_column(justify="left")
+        grid.add_column(justify="center", ratio=1)
+        grid.add_column(justify="right")
+        grid.add_row(
+            "💻", "[b]Anonymous-Corp[/]", "🍞")
+        return Panel(grid, style="green on black")
+    
+layout["Header"].size = 3
+layout["Footer"].size = 3
+layout["Header"].update(Header())
+layout["Footer"].update(Footer())
+
+print(layout)
