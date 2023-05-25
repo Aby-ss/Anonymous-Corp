b8872fad3740648d31cd3d5610b067ff4e8e2f01
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-05-25 17:04:19 +0400

    Changed Button Size ✅

--- Website/script.js
+++ Website/script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     var button = document.createElement("button");
     button.innerHTML = "Click Me";
+    button.style.fontSize = "24px";
+    button.style.padding = "50px 100px";
+    button.style.borderRadius = "6px";
     button.addEventListener("click", function() {
       console.log("Button clicked!");
     });
