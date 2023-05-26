05fd3f9168a911c9f82e438e7d5ee3e513bbfe12
Rao <103417697+CodingAssassins@users.noreply.github.com>
2023-05-27 03:28:49 +0400

    Added Sales Tracking Function ✅

--- Application/sales_tracking.py
+++ Application/sales_tracking.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import csv
 import time
+import random
 import numpy as np
 import asciichartpy
 
@@ -19,3 +20,31 @@ from rich.progress import Progress
 
 from rich.traceback import install
 install(show_locals=True)
+
+# # Print sales graph
+# while True:
+#     graph = asciichartpy.plot(sales_data, {'height': 20})
+#     panel = f"[bold]Product Sales Tracking[/bold]\n\n{graph}"
+#     print(Panel(panel))
+#     time.sleep(1)  # Wait for 1 second before updating the graph
+
+
+
+# Function to generate random sales data
+def generate_sales_data(num_points):
+    sales_data = []
+    sales = 1000  # Initial sales value
+    for _ in range(num_points):
+        change = random.randint(-100, 100)  # Generate random sales change
+        sales += change
+        sales_data.append(sales)
+    return sales_data
+
+# Generate sales data
+num_points = 50  # Number of data points
+sales_data = generate_sales_data(num_points)
+
+# Print sales graph
+graph = asciichartpy.plot(sales_data, {'height': 20})
+panel = f"[bold]Product Sales Tracking[/bold]\n\n{graph}"
+print(Panel(panel, border_style = "bold white", box = box.SQUARE))
